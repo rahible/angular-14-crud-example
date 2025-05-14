15d1258561f5d0e33bebf7b538d4e4cbe58aa25e
Wesley Wexell <wwexell@gmail.com>
2025-05-14 15:22:51 -0500

    chatgpt created table filter

--- src/app/users/list.component.ts
+++ src/app/users/list.component.ts
@@ -1,4 +1,5 @@
-﻿import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
 import { UserService } from '@app/_services';
@@ -6,13 +7,39 @@ import { UserService } from '@app/_services';
 @Component({ templateUrl: 'list.component.html' })
 export class ListComponent implements OnInit {
     users?: any[];
+    filteredUsers?: any[];
+    filterForm: FormGroup;
+    roles: string[] = ['Admin', 'User'];
 
-    constructor(private userService: UserService) {}
+    constructor(private userService: UserService, private fb: FormBuilder) {
+        this.filterForm = this.fb.group({
+            name: [''],
+            role: ['']
+        });
+    }
 
     ngOnInit() {
         this.userService.getAll()
             .pipe(first())
-            .subscribe(users => this.users = users);
+            .subscribe(users => {
+                this.users = users;
+                this.applyFilters();
+            });
+        this.filterForm.valueChanges.subscribe(() => this.applyFilters());
+    }
+
+    applyFilters() {
+        if (!this.users) return;
+        const { name, role } = this.filterForm.value;
+        this.filteredUsers = this.users.filter(user => {
+            const matchesName = name ? (`${user.title} ${user.firstName} ${user.lastName}`.toLowerCase().includes(name.toLowerCase())) : true;
+            const matchesRole = role ? user.role === role : true;
+            return matchesName && matchesRole;
+        });
+    }
+
+    clearFilters() {
+        this.filterForm.reset({ name: '', role: '' });
     }
 
     deleteUser(id: string) {
@@ -20,6 +47,9 @@ export class ListComponent implements OnInit {
         user.isDeleting = true;
         this.userService.delete(id)
             .pipe(first())
-            .subscribe(() => this.users = this.users!.filter(x => x.id !== id));
+            .subscribe(() => {
+                this.users = this.users!.filter(x => x.id !== id);
+                this.applyFilters();
+            });
     }
 }
